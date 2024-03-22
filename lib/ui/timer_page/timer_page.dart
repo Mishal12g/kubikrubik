4c606a1d1677395198afac725839c07b72d1a0c1
Mishal12g <mishal12g@gmail.com>
2024-03-22 14:51:38 +0300

    feat: add picker time

--- lib/ui/timer_page/timer_page.dart
+++ lib/ui/timer_page/timer_page.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:kubikrubik/models/catalog.dart';
+import 'package:kubikrubik/models/enums/timer_stopwatch.dart';
 import 'package:kubikrubik/resources/colors_app.dart';
 import 'package:kubikrubik/resources/resources.dart';
 import 'package:kubikrubik/ui/components/background_image_widget.dart';
+import 'package:kubikrubik/ui/components/button_widget.dart';
 import 'package:kubikrubik/ui/components/container_widget.dart';
 import 'package:kubikrubik/ui/components/form_text_field_widget.dart';
 import 'package:kubikrubik/ui/components/timer_button_widget.dart';
@@ -16,11 +21,25 @@ class TimerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const selectedStyle = TextStyle(
+      decoration: TextDecoration.underline,
+      decorationColor: Colors.white,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+      fontSize: 18,
+    );
+    const notSelectedStyle = TextStyle(
+      color: ColorsApp.grey,
+      fontSize: 16,
+    );
+
     final TextEditingController selectTextFieldController =
         TextEditingController();
     final TextEditingController nameTextFieldController =
         TextEditingController();
+
     Get.lazyPut(() => TimerPageController());
+    final c = Get.find<TimerPageController>();
 
     return Scaffold(
       appBar: AppBar(
@@ -36,45 +55,47 @@ class TimerPage extends StatelessWidget {
               child: ListView(
                 children: [
                   const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      const SizedBox(width: 40),
-                      const Spacer(),
-                      TextButton(
-                        child: const Text(
-                          "Таймер",
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 18,
+                  GetBuilder<TimerPageController>(
+                    builder: (controller) => Row(
+                      children: [
+                        const SizedBox(width: 40),
+                        const Spacer(),
+                        TextButton(
+                          child: Text(
+                            "Таймер",
+                            style: c.timerOrStopwatch == TimerStopwatch.timer
+                                ? selectedStyle
+                                : notSelectedStyle,
+                          ),
+                          onPressed: () {
+                            c.setTimerOrStopwatch(TimerStopwatch.timer);
+                          },
+                        ),
+                        TextButton(
+                          child: Text(
+                            "Секундомер",
+                            style:
+                                c.timerOrStopwatch == TimerStopwatch.stopwatch
+                                    ? selectedStyle
+                                    : notSelectedStyle,
+                          ),
+                          onPressed: () {
+                            c.setTimerOrStopwatch(TimerStopwatch.stopwatch);
+                          },
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Image(
+                              image: AssetImage(AppImages.record),
+                            ),
                           ),
                         ),
-                        onPressed: () {},
-                      ),
-                      TextButton(
-                        child: const Text(
-                          "Секундомер",
-                          style: TextStyle(
-                            color: ColorsApp.grey,
-                            fontSize: 16,
-                          ),
-                        ),
-                        onPressed: () {},
-                      ),
-                      const Spacer(),
-                      SizedBox(
-                        width: 40,
-                        height: 40,
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: const Image(
-                            image: AssetImage(AppImages.record),
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   const SizedBox(height: 40),
                   ContainerWidget(
@@ -97,7 +118,7 @@ class TimerPage extends StatelessWidget {
                         const Divider(
                           color: ColorsApp.blueButton,
                         ),
-                        const MyTimerWidget(),
+                        const _MyTimerWidget(),
                       ],
                     ),
                   ),
@@ -120,6 +141,8 @@ class _PopUpSize extends StatelessWidget {
     TimerPageController c = Get.find<TimerPageController>();
 
     return PopupMenuButton<String>(
+      color: Colors.white,
+      surfaceTintColor: Colors.white,
       icon: const Image(
         image: AssetImage(AppImages.popUpClose),
       ),
@@ -146,33 +169,134 @@ class _PopUpSize extends StatelessWidget {
   }
 }
 
-class MyTimerWidget extends StatelessWidget {
-  const MyTimerWidget({super.key});
+class _MyTimerWidget extends StatelessWidget {
+  const _MyTimerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     TimerPageController c = Get.find<TimerPageController>();
     return GetBuilder<TimerPageController>(
-      builder: (_) => Column(
-        children: [
-          TimerButtonWidget(
-            isPlay: c.isPlay,
-            action: () {
-              c.isPlay = !c.isPlay;
-              c.isPlay ? c.startTimer() : c.stopTimer();
-            },
-          ),
-          Center(
-            child: Text(
-              c.timeResult,
-              style: const TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
-      ),
+      builder: (_) {
+        switch (c.timerOrStopwatch) {
+          case TimerStopwatch.timer:
+            return Column(
+              children: [
+                TimerButtonWidget(
+                  isPlay: c.isPlay,
+                  action: () {
+                    c.isPlay = !c.isPlay;
+                    c.isPlay ? c.startTimer() : c.stopTimer();
+                  },
+                ),
+                Center(
+                  child: Text(
+                    c.timeResult,
+                    style: const TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 130,
+                  child: Column(
+                    children: [
+                      const Divider(
+                        color: ColorsApp.blueButton,
+                      ),
+                      TextButton(
+                        onPressed: () => showCupertinoDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(2.0),
+                                ),
+                              ),
+                              surfaceTintColor: Colors.white,
+                              backgroundColor: Colors.white,
+                              content: SizedBox(
+                                height: 150,
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: CupertinoPicker(
+                                        backgroundColor: Colors.white,
+                                        itemExtent: 32.0,
+                                        onSelectedItemChanged: (value) {
+                                          c.selectTime(value);
+                                        },
+                                        children: c.timeStrings
+                                            .map((e) => Text(e))
+                                            .toList(),
+                                      ),
+                                    ),
+                                    ButtonWidget(
+                                        color: ColorsApp.blue,
+                                        text: const Text(
+                                          "Выбрать",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          Get.back();
+                                        })
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              c.selectedTime,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(width: 15),
+                            const Image(
+                              width: 25,
+                              height: 25,
+                              fit: BoxFit.contain,
+                              image: AssetImage(AppImages.hourglass),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          case TimerStopwatch.stopwatch:
+            return Column(
+              children: [
+                TimerButtonWidget(
+                  isPlay: c.isPlay,
+                  action: () {
+                    c.isPlay = !c.isPlay;
+                    c.isPlay ? c.startTimer() : c.stopTimer();
+                  },
+                ),
+                Center(
+                  child: Text(
+                    c.timeResult,
+                    style: const TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            );
+        }
+      },
     );
   }
 }
--- lib/ui/timer_page/timer_page_controller.dart
+++ lib/ui/timer_page/timer_page_controller.dart
@@ -1,28 +1,64 @@
 import 'dart:async';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:kubikrubik/models/catalog.dart';
+import 'package:kubikrubik/models/enums/timer_stopwatch.dart';
 import 'package:kubikrubik/services/catalogs_services.dart';
 
 class TimerPageController extends GetxController {
   final CatalogService _catalogService = CatalogService();
-  late Timer _timer;
 
   List<Catalog> _catalogs = [];
-
+  TimerStopwatch _timerOrStopwatch = TimerStopwatch.timer;
   String _timeResult = "00:00";
   bool isPlay = false;
   bool timerIsActive = false;
+  late String selectedTime;
 
   String get timeResult => _timeResult;
+  TimerStopwatch get timerOrStopwatch => _timerOrStopwatch;
   List<Catalog> get catalogs => _catalogs;
+
+  late Timer _timer;
+
+  final List<String> timeStrings = [
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00",
+    "10:00",
+  ];
 
   TimerPageController() {
     loadCatalogs();
+    selectTime(0);
+  }
+
+  setTimerOrStopwatch(TimerStopwatch timerStopwatch) {
+    if (!isPlay) {
+      switch (_timerOrStopwatch) {
+        case TimerStopwatch.timer:
+          _timerOrStopwatch = timerStopwatch;
+        case TimerStopwatch.stopwatch:
+          _timerOrStopwatch = timerStopwatch;
+      }
+      _timeResult = "00:00";
+      update();
+    }
   }
 
   loadCatalogs() {
     _catalogService.load();
     _catalogs = _catalogService.catalogs;
+  }
+
+  selectTime(index) {
+    selectedTime = timeStrings[index];
+    update();
   }
 
   String _formatDuration(int seconds) {
